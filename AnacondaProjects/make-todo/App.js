bafe54ebad7e41f149c483c852ea09d5bc3b7155
SY <likerberi97@gmail.com>
2018-03-16 15:19:55 +0900

    UI step1

--- AnacondaProjects/make-todo/App.js
+++ AnacondaProjects/make-todo/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Dimensions } from 'react-native';
+
+const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content"/>
+        <Text style={styles.title}>샤로수 매니저</Text>
+        <View style={styles.card}>
+          <Text style={styles.input} placeholder={"New to do"}/>
+        </View>
       </View>
     );
   }
@@ -14,8 +20,26 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: "white",
+    fontSize: 30,
+    marginTop: 30,
+    fontWeight: "300",
+    marginBottom: 10,  
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width-30,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    elevation: 3
+  },
+  input: {
+    color: "skyblue",
+  }
 });
